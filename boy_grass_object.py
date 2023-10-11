d723ad73f9da6e45b7544857da937b87d673e88e
pikach-u <ziwon05@naver.com>
2023-10-11 22:58:58 +0900

    Class Grass와 Boy를 만들고 list를 통해 Boy 객체를 여러 개 생성. 모든 소년의 시작 위치가 같다는 문제점이 있음.

---
+++ boy_grass_object.py
@@ -0,0 +1,77 @@
+from pico2d import *
+
+
+
+# Game object class here
+class Grass:
+    def __init__(self): #객체가 생성될 때 맨 처음 자동 호출되는 함수. 객체의 초기 상태를 결정
+        self.image = load_image('grass.png')
+
+    def draw(self): #self-생성된 객체를 가리키는 더미 변수. 멤버 함수는 항상 첫 번째 인자가 self 여야 함
+        self.image.draw(400,30)
+
+    def update(self):
+        pass
+
+class Boy:
+    def __init__(self):
+        self.x, self.y = 0, 90
+        self.frame = 0
+        self.image = load_image('run_animation.png')
+
+    def update(self):
+        self.frame = (self.frame + 1) % 8
+        self.x += 5
+
+    def draw(self):
+        self.image.clip_draw(self.frame*100, 0, 100, 100, self.x, self.y)
+
+def handle_events():
+    global running
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
+
+
+def reset_world():
+    global running
+    global grass
+    global team
+
+    running = True
+    grass = Grass()
+    team = [Boy() for i in range(10)]
+
+def update_world():
+    grass.update()
+    for boy in team:
+        boy.update()
+    pass
+
+
+def render_world():
+    clear_canvas()
+    grass.draw()
+    for boy in team:
+        boy.draw()
+    update_canvas()
+
+
+open_canvas()
+
+# initialization code
+reset_world()
+
+# game main loop code
+while running:
+    handle_events()
+    update_world()
+    render_world()
+    delay(0.05)
+
+# finalization code
+
+close_canvas()
